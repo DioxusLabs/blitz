4a3a09efa5f8719deb76ea64726ac61bf6776562
Jonathan Kelley <jkelleyrtp@gmail.com>
2023-12-30 01:51:06 -0500

    fix imports on example

--- examples/outline.rs
+++ examples/outline.rs
@@ -1,7 +1,7 @@
 // background: rgb(2,0,36);
 // background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
 
-use dioxus::prelude::{GlobalAttributes, SvgAttributes, *};
+use dioxus::prelude::*;
 use stylo_dioxus::Config;
 
 fn main() {
