695888cc21f9afcb0169e0a86fdb274a0a76fdfd
Nico Burns <nico@nicoburns.com>
2024-03-13 12:16:14 +1300

    Move walk_rc_dom function to utils module

--- packages/dom/src/document.rs
+++ packages/dom/src/document.rs
@@ -50,48 +50,6 @@ pub struct Document {
     pub(crate) nodes_to_id: HashMap<String, usize>,
 }
 
-fn walk_rc_dom(indent: usize, handle: &Handle) {
-    let node = handle;
-    for _ in 0..indent {
-        print!(" ");
-    }
-    match node.data {
-        NodeData::Document => println!("#Document"),
-
-        NodeData::Doctype {
-            ref name,
-            ref public_id,
-            ref system_id,
-        } => println!("<!DOCTYPE {} \"{}\" \"{}\">", name, public_id, system_id),
-
-        NodeData::Text { ref contents } => {
-            println!("#text: {}", contents.borrow().escape_default())
-        }
-
-        NodeData::Comment { ref contents } => println!("<!-- {} -->", contents.escape_default()),
-
-        NodeData::Element {
-            ref name,
-            ref attrs,
-            ..
-        } => {
-            // assert!(name.ns == ns!(html));
-            print!("<{}", name.local);
-            for attr in attrs.borrow().iter() {
-                // assert!(attr.name.ns == ns!());
-                print!(" {}=\"{}\"", attr.name.local, attr.value);
-            }
-            println!(">");
-        }
-
-        NodeData::ProcessingInstruction { .. } => unreachable!(),
-    }
-
-    for child in node.children.borrow().iter() {
-        walk_rc_dom(indent + 4, child);
-    }
-}
-
 impl Document {
     pub fn new(device: Device) -> Self {
         let quirks = QuirksMode::NoQuirks;
@@ -146,7 +104,7 @@ impl Document {
 
         // Debug print RcDom
         //
-        // walk_rc_dom(2, &document.document);
+        // crate::util::walk_rc_dom(2, &document.document);
         // if !document.errors.is_empty() {
         //     println!("\nParse errors:");
         //     for err in document.errors.iter() {
--- packages/dom/src/util.rs
+++ packages/dom/src/util.rs
@@ -1,5 +1,7 @@
 use std::io::Read;
 
+use markup5ever_rcdom::{Handle, NodeData};
+
 const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0";
 const FILE_SIZE_LIMIT: u64 = 1_000_000_000; // 1GB
 
@@ -24,4 +26,47 @@ pub(crate) fn fetch_blob(url: &str) -> Result<Vec<u8>, ureq::Error> {
         .read_to_end(&mut bytes)?;
 
     Ok(bytes)
+}
+
+// Debug print an RcDom
+pub (crate) fn walk_rc_dom(indent: usize, handle: &Handle) {
+    let node = handle;
+    for _ in 0..indent {
+        print!(" ");
+    }
+    match node.data {
+        NodeData::Document => println!("#Document"),
+
+        NodeData::Doctype {
+            ref name,
+            ref public_id,
+            ref system_id,
+        } => println!("<!DOCTYPE {} \"{}\" \"{}\">", name, public_id, system_id),
+
+        NodeData::Text { ref contents } => {
+            println!("#text: {}", contents.borrow().escape_default())
+        }
+
+        NodeData::Comment { ref contents } => println!("<!-- {} -->", contents.escape_default()),
+
+        NodeData::Element {
+            ref name,
+            ref attrs,
+            ..
+        } => {
+            // assert!(name.ns == ns!(html));
+            print!("<{}", name.local);
+            for attr in attrs.borrow().iter() {
+                // assert!(attr.name.ns == ns!());
+                print!(" {}=\"{}\"", attr.name.local, attr.value);
+            }
+            println!(">");
+        }
+
+        NodeData::ProcessingInstruction { .. } => unreachable!(),
+    }
+
+    for child in node.children.borrow().iter() {
+        walk_rc_dom(indent + 4, child);
+    }
 }
