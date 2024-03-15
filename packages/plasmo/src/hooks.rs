e213444e317ff98591f27fc16df3b7a86a506c93
Jonathan Kelley <jkelleyrtp@gmail.com>
2024-03-14 18:39:49 -0700

    Add workspace deps for dioxus

--- packages/plasmo/src/hooks.rs
+++ packages/plasmo/src/hooks.rs
@@ -66,7 +66,7 @@ impl EventData {
 pub struct FormData {
     pub(crate) value: String,
 
-    pub values: HashMap<String, String>,
+    pub values: HashMap<String, FormValue>,
 
     pub(crate) files: Option<Files>,
 }
@@ -76,7 +76,7 @@ impl HasFormData for FormData {
         self.value.clone()
     }
 
-    fn values(&self) -> HashMap<String, String> {
+    fn values(&self) -> HashMap<String, FormValue> {
         self.values.clone()
     }
 
