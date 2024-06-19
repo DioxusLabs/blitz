cf48112bb6091f77e0812574b8376fb9b67aa7bb
Matt Hunzinger <matthunz2@gmail.com>
2024-06-18 21:06:07 -0400

    Replace tao with winit

--- packages/dioxus-blitz/src/lib.rs
+++ packages/dioxus-blitz/src/lib.rs
@@ -11,13 +11,13 @@ use dioxus::prelude::*;
 use documents::DioxusDocument;
 use muda::{MenuEvent, MenuId};
 use std::collections::HashMap;
-use tao::event_loop::EventLoopBuilder;
-use tao::window::WindowId;
-use tao::{
+use url::Url;
+use winit::event_loop::{EventLoop, EventLoopBuilder};
+use winit::window::WindowId;
+use winit::{
     event::{Event, WindowEvent},
     event_loop::ControlFlow,
 };
-use url::Url;
 
 #[derive(Default)]
 pub struct Config {
@@ -93,7 +93,9 @@ fn launch_with_window<Doc: DocumentLike + 'static>(window: View<'static, Doc>) {
     let _guard = rt.enter();
 
     // Build an event loop for the application
-    let event_loop = EventLoopBuilder::<UserWindowEvent>::with_user_event().build();
+    let event_loop = EventLoop::<UserWindowEvent>::with_user_event()
+        .build()
+        .unwrap();
     let proxy = event_loop.create_proxy();
 
     // Multiwindow ftw
@@ -106,94 +108,96 @@ fn launch_with_window<Doc: DocumentLike + 'static>(window: View<'static, Doc>) {
     #[cfg(not(any(target_os = "android", target_os = "ios")))]
     let mut initial = true;
 
-    event_loop.run(move |event, event_loop, control_flow| {
-        *control_flow = ControlFlow::Wait;
+    event_loop
+        .run(move |event, event_loop| {
+            event_loop.set_control_flow(ControlFlow::Wait);
 
-        let mut on_resume = || {
-            for (_, view) in windows.iter_mut() {
-                view.resume(event_loop, &proxy, &rt);
+            let mut on_resume = || {
+                for (_, view) in windows.iter_mut() {
+                    view.resume(event_loop, &proxy, &rt);
+                }
+
+                for view in pending_windows.iter_mut() {
+                    view.resume(event_loop, &proxy, &rt);
+                }
+
+                for window in pending_windows.drain(..) {
+                    let RenderState::Active(state) = &window.renderer.render_state else {
+                        continue;
+                    };
+                    windows.insert(state.window.id(), window);
+                }
+            };
+
+            #[cfg(not(any(target_os = "android", target_os = "ios")))]
+            if initial {
+                on_resume();
+                initial = false;
             }
 
-            for view in pending_windows.iter_mut() {
-                view.resume(event_loop, &proxy, &rt);
+            match event {
+                // Exit the app when close is request
+                // Not always necessary
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => event_loop.exit(),
+
+                Event::WindowEvent {
+                    window_id,
+                    event: winit::event::WindowEvent::RedrawRequested,
+                } => {
+                    if let Some(window) = windows.get_mut(&window_id) {
+                        window.renderer.dom.as_mut().resolve();
+                        window.renderer.render(&mut window.scene);
+                    };
+                }
+
+                Event::UserEvent(UserWindowEvent(EventData::Poll, id)) => {
+                    if let Some(view) = windows.get_mut(&id) {
+                        if view.poll() {
+                            view.request_redraw();
+                        }
+                    };
+                }
+                // Event::UserEvent(_redraw) => {
+                //     for (_, view) in windows.iter() {
+                //         view.request_redraw();
+                //     }
+                // }
+                Event::NewEvents(_) => {
+                    for id in windows.keys() {
+                        _ = proxy.send_event(UserWindowEvent(EventData::Poll, *id));
+                    }
+                }
+
+                Event::Suspended => {
+                    for (_, view) in windows.iter_mut() {
+                        view.suspend();
+                    }
+                }
+
+                Event::Resumed => on_resume(),
+
+                Event::WindowEvent {
+                    window_id, event, ..
+                } => {
+                    if let Some(window) = windows.get_mut(&window_id) {
+                        window.handle_window_event(event);
+                    };
+                }
+
+                _ => (),
             }
 
-            for window in pending_windows.drain(..) {
-                let RenderState::Active(state) = &window.renderer.render_state else {
-                    continue;
-                };
-                windows.insert(state.window.id(), window);
-            }
-        };
-
-        #[cfg(not(any(target_os = "android", target_os = "ios")))]
-        if initial {
-            on_resume();
-            initial = false;
-        }
-
-        match event {
-            // Exit the app when close is request
-            // Not always necessary
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => *control_flow = ControlFlow::Exit,
-
-            // Nothing else to do, try redrawing?
-            Event::MainEventsCleared => {}
-
-            Event::UserEvent(UserWindowEvent(EventData::Poll, id)) => {
-                if let Some(view) = windows.get_mut(&id) {
-                    if view.poll() {
+            if let Ok(event) = menu_channel.try_recv() {
+                if event.id == MenuId::new("dev.show_layout") {
+                    for (_, view) in windows.iter_mut() {
+                        view.renderer.devtools.show_layout = !view.renderer.devtools.show_layout;
                         view.request_redraw();
                     }
-                };
-            }
-            // Event::UserEvent(_redraw) => {
-            //     for (_, view) in windows.iter() {
-            //         view.request_redraw();
-            //     }
-            // }
-            Event::NewEvents(_) => {
-                for id in windows.keys() {
-                    _ = proxy.send_event(UserWindowEvent(EventData::Poll, *id));
                 }
             }
-
-            Event::RedrawRequested(window_id) => {
-                if let Some(window) = windows.get_mut(&window_id) {
-                    window.renderer.dom.as_mut().resolve();
-                    window.renderer.render(&mut window.scene);
-                };
-            }
-
-            Event::Suspended => {
-                for (_, view) in windows.iter_mut() {
-                    view.suspend();
-                }
-            }
-
-            Event::Resumed => on_resume(),
-
-            Event::WindowEvent {
-                window_id, event, ..
-            } => {
-                if let Some(window) = windows.get_mut(&window_id) {
-                    window.handle_window_event(event);
-                };
-            }
-
-            _ => (),
-        }
-
-        if let Ok(event) = menu_channel.try_recv() {
-            if event.id == MenuId::new("dev.show_layout") {
-                for (_, view) in windows.iter_mut() {
-                    view.renderer.devtools.show_layout = !view.renderer.devtools.show_layout;
-                    view.request_redraw();
-                }
-            }
-        }
-    });
+        })
+        .unwrap();
 }
