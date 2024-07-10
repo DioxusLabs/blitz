34a8e00f285e958733349dbae0f7883fe22eb526
Matt Hunzinger <matthunz2@gmail.com>
2024-07-09 22:01:25 -0400

    Create blitz-masonry crate

---
+++ packages/blitz-masonry/src/lib.rs
@@ -0,0 +1,42 @@
+#![windows_subsystem = "windows"]
+
+use masonry::app_driver::{AppDriver, DriverCtx};
+use masonry::dpi::LogicalSize;
+use masonry::widget::{Button, Flex, Label, RootWidget};
+use masonry::{Action, WidgetId};
+use winit::window::Window;
+
+struct Driver;
+
+impl AppDriver for Driver {
+    fn on_action(&mut self, _ctx: &mut DriverCtx<'_>, _widget_id: WidgetId, action: Action) {
+        match action {
+            Action::ButtonPressed(_) => {
+                println!("Hello");
+            }
+            action => {
+                eprintln!("Unexpected action {action:?}");
+            }
+        }
+    }
+}
+
+pub fn main() {
+    let label = Label::new("Hello").with_text_size(32.0);
+    
+    let main_widget = Flex::column()
+        .with_child(label);
+
+    let window_size = LogicalSize::new(600.0, 400.0);
+    let window_attributes = Window::default_attributes()
+        .with_title("Blitz")
+        .with_min_inner_size(window_size);
+
+    masonry::event_loop_runner::run(
+        masonry::event_loop_runner::EventLoop::with_user_event(),
+        window_attributes,
+        RootWidget::new(main_widget),
+        Driver,
+    )
+    .unwrap();
+}
