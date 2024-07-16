b541639d7c3fbb2f4df36e051315fc7989f3442a
Nico Burns <nico@nicoburns.com>
2024-07-16 16:40:01 +1200

    cargo fmt

--- packages/dom/src/stylo_to_taffy.rs
+++ packages/dom/src/stylo_to_taffy.rs
@@ -5,9 +5,9 @@ mod stylo {
     pub(crate) use style::computed_values::flex_direction::T as FlexDirection;
     pub(crate) use style::computed_values::flex_wrap::T as FlexWrap;
     pub(crate) use style::computed_values::grid_auto_flow::T as GridAutoFlow;
+    pub(crate) use style::properties::generated::longhands::box_sizing::computed_value::T as BoxSizing;
     pub(crate) use style::properties::longhands::aspect_ratio::computed_value::T as AspectRatio;
     pub(crate) use style::properties::longhands::position::computed_value::T as Position;
-    pub(crate) use style::properties::generated::longhands::box_sizing::computed_value::T as BoxSizing;
     pub(crate) use style::properties::style_structs::{Margin, Padding};
     pub(crate) use style::values::computed::GridLine;
     pub(crate) use style::values::computed::GridTemplateComponent;
@@ -105,7 +105,7 @@ pub(crate) fn border(
     }
 }
 
-pub (crate) fn box_sizing(input: stylo::BoxSizing) -> taffy::BoxSizing {
+pub(crate) fn box_sizing(input: stylo::BoxSizing) -> taffy::BoxSizing {
     match input {
         stylo::BoxSizing::BorderBox => taffy::BoxSizing::BorderBox,
         stylo::BoxSizing::ContentBox => taffy::BoxSizing::ContentBox,
