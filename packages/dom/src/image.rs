7d164ef3b20eecfc60926852ba6704800e91f460
Nico Burns <nico@nicoburns.com>
2024-07-23 14:42:09 +1200

    Revert image measure function changes

--- packages/dom/src/image.rs
+++ packages/dom/src/image.rs
@@ -3,7 +3,6 @@ use taffy::{MaybeMath, MaybeResolve};
 #[derive(Debug, Clone, Copy)]
 pub struct ImageContext {
     pub inherent_size: taffy::Size<f32>,
-    pub inherent_aspect_ratio: Option<f32>,
     pub attr_size: taffy::Size<Option<f32>>,
 }
 
@@ -18,47 +17,48 @@ pub fn image_measure_function(
 
     // Use aspect_ratio from style, fall back to inherent aspect ratio
     let s_aspect_ratio = style.aspect_ratio;
-    let aspect_ratio = s_aspect_ratio.or(image_context.inherent_aspect_ratio);
+    let aspect_ratio = s_aspect_ratio.unwrap_or_else(|| inherent_size.width / inherent_size.height);
 
     // Resolve sizes
     let style_size = style
         .size
         .maybe_resolve(parent_size)
-        .maybe_apply_aspect_ratio(aspect_ratio);
+        .maybe_apply_aspect_ratio(Some(aspect_ratio));
     let min_size = style
         .min_size
         .maybe_resolve(parent_size)
-        .maybe_apply_aspect_ratio(aspect_ratio);
+        .maybe_apply_aspect_ratio(Some(aspect_ratio));
     let max_size = style
         .max_size
         .maybe_resolve(parent_size)
-        .maybe_apply_aspect_ratio(aspect_ratio);
+        .maybe_apply_aspect_ratio(Some(aspect_ratio));
     let attr_size = image_context
         .attr_size
-        .maybe_apply_aspect_ratio(aspect_ratio);
+        .maybe_apply_aspect_ratio(Some(aspect_ratio));
 
-    let size = known_dimensions.maybe_apply_aspect_ratio(aspect_ratio);
-    if let (Some(width), Some(height)) = (size.width, size.height) {
-        return taffy::Size { width, height };
+    if known_dimensions.width.is_some() | known_dimensions.height.is_some() {
+        return known_dimensions
+            .maybe_apply_aspect_ratio(Some(aspect_ratio))
+            .map(|s| s.unwrap());
     }
 
-    let size = style_size
-        .maybe_clamp(min_size, max_size)
-        .maybe_apply_aspect_ratio(aspect_ratio);
-    if let (Some(width), Some(height)) = (size.width, size.height) {
-        return taffy::Size { width, height };
+    if style_size.width.is_some() | style_size.height.is_some() {
+        return style_size
+            .maybe_clamp(min_size, max_size)
+            .maybe_apply_aspect_ratio(Some(aspect_ratio))
+            .map(|s| s.unwrap());
     }
 
-    let size = attr_size
-        .maybe_clamp(min_size, max_size)
-        .maybe_apply_aspect_ratio(aspect_ratio);
-    if let (Some(width), Some(height)) = (size.width, size.height) {
-        return taffy::Size { width, height };
+    if attr_size.width.is_some() | attr_size.height.is_some() {
+        return attr_size
+            .maybe_clamp(min_size, max_size)
+            .maybe_apply_aspect_ratio(Some(aspect_ratio))
+            .map(|s| s.unwrap());
     }
 
     inherent_size
         .maybe_clamp(min_size, max_size)
         .map(Some)
-        .maybe_apply_aspect_ratio(aspect_ratio)
+        .maybe_apply_aspect_ratio(Some(aspect_ratio))
         .map(|s| s.unwrap())
 }
