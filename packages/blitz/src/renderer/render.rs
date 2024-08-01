dc21bbb57853912ce0e91bf91594aeb8ec54777b
Nico Burns <nico@nicoburns.com>
2024-08-01 15:27:02 +1200

    Disable clip log

--- packages/blitz/src/renderer/render.rs
+++ packages/blitz/src/renderer/render.rs
@@ -75,12 +75,12 @@ pub fn generate_vello_scene(
     };
     generator.generate_vello_scene(scene);
 
-    println!(
-        "Rendered using {} clips (depth: {}) (wanted: {})",
-        CLIPS_USED.load(atomic::Ordering::SeqCst),
-        CLIP_DEPTH_USED.load(atomic::Ordering::SeqCst),
-        CLIPS_WANTED.load(atomic::Ordering::SeqCst)
-    );
+    // println!(
+    //     "Rendered using {} clips (depth: {}) (wanted: {})",
+    //     CLIPS_USED.load(atomic::Ordering::SeqCst),
+    //     CLIP_DEPTH_USED.load(atomic::Ordering::SeqCst),
+    //     CLIPS_WANTED.load(atomic::Ordering::SeqCst)
+    // );
 }
 
 /// A short-lived struct which holds a bunch of parameters for rendering a vello scene so
