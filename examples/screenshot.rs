c49bd84485033febe510c684d803101dddadd0de
Nico Burns <nico@nicoburns.com>
2024-08-14 17:23:50 +1200

    Generate "full height" screenshots in screenshot example

--- examples/screenshot.rs
+++ examples/screenshot.rs
@@ -1,10 +1,11 @@
 //! Load first CLI argument as a url. Fallback to google.com if no CLI argument is provided.
 
-use std::{fs::File, path::Path};
-
 use blitz::render_to_buffer;
 use blitz_dom::{HtmlDocument, Viewport};
 use reqwest::Url;
+use std::{fs::File, io::Write, path::Path};
+
+const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0";
 
 #[tokio::main]
 async fn main() {
@@ -13,13 +14,13 @@ async fn main() {
         .next()
         .unwrap_or_else(|| "https://www.google.com".into());
 
-    const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0";
     println!("{}", url);
 
     // Assert that url is valid
     let url = url.to_owned();
     Url::parse(&url).expect("Invalid url");
 
+    // Fetch HTML from URL
     let html = ureq::get(&url)
         .set("User-Agent", USER_AGENT)
         .call()
@@ -27,38 +28,59 @@ async fn main() {
         .into_string()
         .unwrap();
 
+    // Setup viewport. TODO: make configurable.
     let scale = 2;
-    let width = 800 * scale;
-    let height = 600 * scale;
+    let width = 1200;
+    let height = 800;
 
-    let viewport = Viewport::new(width, height, scale as f32);
+    // Create HtmlDocument
     let mut document = HtmlDocument::from_html(&html, Some(url), Vec::new());
-    document.as_mut().set_viewport(viewport.clone());
+    document
+        .as_mut()
+        .set_viewport(Viewport::new(width * scale, height * scale, scale as f32));
+
+    // Compute style, layout, etc for HtmlDocument
     document.as_mut().resolve();
 
-    let buffer = render_to_buffer(document.as_ref(), viewport).await;
+    // Determine height to render
+    let computed_height = document.as_ref().root_element().final_layout.size.height;
+    let render_height = (computed_height as u32).max(height);
 
+    // Render document to RGBA buffer
+    let buffer = render_to_buffer(
+        document.as_ref(),
+        Viewport::new(width * scale, render_height * scale, scale as f32),
+    )
+    .await;
+
+    // Determine output path, and open a file at that path. TODO: make configurable.
     let out_path = Path::new(env!("CARGO_MANIFEST_DIR"))
         .join("examples/output/screenshot")
         .with_extension("png");
 
-    write_png(out_path.as_path(), &buffer, width, height);
+    // Encode buffer as PNG and write it to a file
+    let mut file = File::create(&out_path).unwrap();
+    write_png(&mut file, &buffer, width * scale, render_height * scale);
 
-    println!("Wrote result ({width}x{height}) to {out_path:?}");
+    // Log result.
+    println!("Wrote result ({width}x{height}) to {}", out_path.display());
 }
 
-fn write_png(out_path: &Path, buffer: &[u8], width: u32, height: u32) {
-    let mut file = File::create(&out_path).unwrap();
+fn write_png<W: Write>(writer: W, buffer: &[u8], width: u32, height: u32) {
+    // Set pixels-per-meter. TODO: make configurable.
+    const PPM: u32 = (144.0 * 39.3701) as u32;
 
-    let mut encoder = png::Encoder::new(&mut file, width, height);
+    // Create PNG encoder
+    let mut encoder = png::Encoder::new(writer, width, height);
     encoder.set_color(png::ColorType::Rgba);
     encoder.set_depth(png::BitDepth::Eight);
-    const PPM: u32 = (144.0 * 39.3701) as u32;
     encoder.set_pixel_dims(Some(png::PixelDimensions {
         xppu: PPM,
         yppu: PPM,
         unit: png::Unit::Meter,
     }));
+
+    // Write PNG data to writer
     let mut writer = encoder.write_header().unwrap();
     writer.write_image_data(&buffer).unwrap();
     writer.finish().unwrap();
