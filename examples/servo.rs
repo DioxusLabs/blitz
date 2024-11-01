b4a3e1bac7824574802a9083712e1f545cd0a4b8
koko <kokos.kekse@gmail.com>
2024-11-01 19:24:49 +0100

    Fix the servo example
    Signed-off-by: koko <kokos.kekse@gmail.com>

--- examples/servo.rs
+++ examples/servo.rs
@@ -1,11 +1,14 @@
 use dioxus_native::Config;
+use std::path::Path;
 
 fn main() {
+    let dir = Path::new(env!("CARGO_MANIFEST_DIR")).join("examples/assets/servo.html");
+    let url = format!("file://{}", dir.display());
     dioxus_native::launch_static_html_cfg(
         include_str!("./assets/servo.html"),
         Config {
             stylesheets: Vec::new(),
-            base_url: Some(String::from("https://servo.org/")),
+            base_url: Some(url),
         },
     );
 }
