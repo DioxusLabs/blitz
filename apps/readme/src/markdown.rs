240f15cbf1f7f96e67127809a3a58aab2f9601a9
Nico Burns <nico@nicoburns.com>
2024-12-16 23:37:53 +1300

    Disable syntect in readme app
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- apps/readme/src/markdown.rs
+++ apps/readme/src/markdown.rs
@@ -1,11 +1,6 @@
 //! Render the readme.md using the gpu renderer
 
-use std::collections::HashMap;
-
-use comrak::{
-    adapters::SyntaxHighlighterAdapter, markdown_to_html_with_plugins,
-    plugins::syntect::SyntectAdapter, ExtensionOptions, Options, Plugins, RenderOptions,
-};
+use comrak::{markdown_to_html_with_plugins, ExtensionOptions, Options, Plugins, RenderOptions};
 
 pub(crate) const GITHUB_MD_STYLES: &str = include_str!("../assets/github-markdown.css");
 pub(crate) const BLITZ_MD_STYLES: &str = include_str!("../assets/blitz-markdown-overrides.css");
@@ -53,33 +48,39 @@ pub(crate) fn markdown_to_html(contents: String) -> String {
     )
 }
 
-#[allow(unused)]
-struct CustomSyntectAdapter(SyntectAdapter);
+// #[allow(unused)]
+// mod syntax_highlighter {
+//     use comrak::adapters::SyntaxHighlighterAdapter;
+//     use comrak::plugins::syntect::SyntectAdapter;
+//     use std::collections::HashMap;
 
-impl SyntaxHighlighterAdapter for CustomSyntectAdapter {
-    fn write_highlighted(
-        &self,
-        output: &mut dyn std::io::Write,
-        lang: Option<&str>,
-        code: &str,
-    ) -> std::io::Result<()> {
-        let norm_lang = lang.map(|l| l.split_once(',').map(|(lang, _)| lang).unwrap_or(l));
-        self.0.write_highlighted(output, norm_lang, code)
-    }
+//     struct CustomSyntectAdapter(SyntectAdapter);
 
-    fn write_pre_tag(
-        &self,
-        output: &mut dyn std::io::Write,
-        attributes: HashMap<String, String>,
-    ) -> std::io::Result<()> {
-        self.0.write_pre_tag(output, attributes)
-    }
+//     impl SyntaxHighlighterAdapter for CustomSyntectAdapter {
+//         fn write_highlighted(
+//             &self,
+//             output: &mut dyn std::io::Write,
+//             lang: Option<&str>,
+//             code: &str,
+//         ) -> std::io::Result<()> {
+//             let norm_lang = lang.map(|l| l.split_once(',').map(|(lang, _)| lang).unwrap_or(l));
+//             self.0.write_highlighted(output, norm_lang, code)
+//         }
 
-    fn write_code_tag(
-        &self,
-        output: &mut dyn std::io::Write,
-        attributes: HashMap<String, String>,
-    ) -> std::io::Result<()> {
-        self.0.write_code_tag(output, attributes)
-    }
-}
+//         fn write_pre_tag(
+//             &self,
+//             output: &mut dyn std::io::Write,
+//             attributes: HashMap<String, String>,
+//         ) -> std::io::Result<()> {
+//             self.0.write_pre_tag(output, attributes)
+//         }
+
+//         fn write_code_tag(
+//             &self,
+//             output: &mut dyn std::io::Write,
+//             attributes: HashMap<String, String>,
+//         ) -> std::io::Result<()> {
+//             self.0.write_code_tag(output, attributes)
+//         }
+//     }
+// }
