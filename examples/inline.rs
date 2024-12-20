eff640c324462d639021cf2545f8143b85e9a8bd
Nico Burns <nico@nicoburns.com>
2024-12-21 12:37:43 +1300

    Delete and combine examples
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- examples/inline.rs
+++ examples/inline.rs
@@ -34,6 +34,55 @@ fn app() -> Element {
                 span { class: "c", "venenatis" }
                 " gravida nisl sit amet facilisis. Nullam cursus fermentum velit sed laoreet. "
             }
+            div { id: "a",
+                "Some text"
+                em { "Another block of text" }
+                "Should connect no space between"
+            }
+            h1 { "ul" }
+            ul {
+                li { "Item 1" }
+                li { "Item 2" }
+                li {
+                    class: "square",
+                    "Square item"
+                }
+                li {
+                    class: "circle",
+                    "Circle item"
+                }
+                li {
+                    class: "disclosure-open",
+                    "Disclosure open item"
+                }
+                li {
+                    class: "disclosure-closed",
+                    "Disclosure closed item"
+                }
+            }
+            h1 { "ol - decimal" }
+            ol {
+                li { "Item 1" }
+                li { "Item 2" }
+                li {
+                    ul {
+                        li { "Nested Item 1" }
+                        li { "Nested Item 2" }
+                    }
+                }
+                li { "Item 3" }
+                li { "Item 4" }
+                ol {
+                    li { "Sub 1" }
+                    li { "Sub 2" }
+                }
+            }
+            h1 { "ol - alpha" }
+            ol { class: "alpha",
+                li { "Item 1" }
+                li { "Item 2" }
+                li { "Item 3" }
+            }
         }
     }
 }
@@ -65,4 +114,26 @@ span.c {
   border: 1px solid blue;
   background-color: yellow;
 }
+
+#a {
+}
+h1 {
+    font-size: 20px;
+}
+ol.alpha {
+    list-style-type: lower-alpha;
+}
+li.square {
+    list-style-type: square;
+}
+li.circle {
+    list-style-type: circle;
+}
+li.disclosure-open {
+    list-style-type: disclosure-open;
+}
+li.disclosure-closed {
+    list-style-type: disclosure-closed;
+}
+
 "#;
--- examples/parse_debug.rs
+++
@@ -1,5 +0,0 @@
-//! Render minimal html5 page
-
-fn main() {
-    blitz_shell::launch_static_html(include_str!("./assets/google_reduced.html"));
-}
