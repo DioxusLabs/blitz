36705688b31a7a8e75bdb49f350c3afd59b5a345
koko <kokos.kekse@gmail.com>
2025-01-07 06:05:56 +0100

    Use a custom Request type instead of url in fetch.

--- packages/blitz-traits/src/net.rs
+++ packages/blitz-traits/src/net.rs
@@ -1,5 +1,5 @@
 pub use bytes::Bytes;
-pub use http::{self, request::Builder, Method, Request};
+pub use http::{self, HeaderMap, Method};
 use std::marker::PhantomData;
 use std::sync::Arc;
 pub use url::Url;
@@ -13,7 +13,7 @@ pub type SharedCallback<D> = Arc<dyn NetCallback<Data = D>>;
 /// This may be over the network via http(s), via the filesystem, or some other method.
 pub trait NetProvider: Send + Sync + 'static {
     type Data;
-    fn fetch(&self, doc_id: usize, url: Url, handler: BoxedHandler<Self::Data>);
+    fn fetch(&self, doc_id: usize, request: Request, handler: BoxedHandler<Self::Data>);
 }
 
 /// A type that parses raw bytes from a network request into a Self::Data and then calls
@@ -21,10 +21,6 @@ pub trait NetProvider: Send + Sync + 'static {
 pub trait NetHandler: Send + Sync + 'static {
     type Data;
     fn bytes(self: Box<Self>, doc_id: usize, bytes: Bytes, callback: SharedCallback<Self::Data>);
-    /// Builds the request used to fetch the data
-    fn request(&self, request: Builder) -> http::Result<Request<Bytes>> {
-        request.method(Method::GET).body(Bytes::new())
-    }
 }
 
 /// A type which accepts the parsed result of a network request and sends it back to the Document
@@ -32,6 +28,25 @@ pub trait NetHandler: Send + Sync + 'static {
 pub trait NetCallback: Send + Sync + 'static {
     type Data;
     fn call(&self, doc_id: usize, data: Self::Data);
+}
+
+#[non_exhaustive]
+#[derive(Debug)]
+pub struct Request {
+    pub url: Url,
+    pub method: Method,
+    pub headers: HeaderMap,
+    pub body: Bytes,
+}
+impl Request {
+    pub fn get(url: Url) -> Self {
+        Self {
+            url,
+            method: Method::GET,
+            headers: HeaderMap::new(),
+            body: Bytes::new(),
+        }
+    }
 }
 
 /// A default noop NetProvider
@@ -43,7 +58,7 @@ impl<D> Default for DummyNetProvider<D> {
 }
 impl<D: Send + Sync + 'static> NetProvider for DummyNetProvider<D> {
     type Data = D;
-    fn fetch(&self, _doc_id: usize, _url: Url, _handler: BoxedHandler<D>) {}
+    fn fetch(&self, _doc_id: usize, _request: Request, _handler: BoxedHandler<D>) {}
 }
 
 /// A default noop NetCallback
