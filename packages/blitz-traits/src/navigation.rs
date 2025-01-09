0f0c42d3412d5f7b7c69c13091edc3e227e9ef78
koko <kokos.kekse@gmail.com>
2025-01-09 03:22:39 +0100

    Navigator options

--- packages/blitz-traits/src/navigation.rs
+++ packages/blitz-traits/src/navigation.rs
@@ -1,12 +1,50 @@
+use url::Url;
+
 /// A provider to enable a document to bubble up navigation events (e.g. clicking a link)
 pub trait NavigationProvider: Send + Sync + 'static {
-    fn navigate_new_page(&self, url: String);
+    fn navigate_new_page(&self, navigation: NavigationOptions);
 }
 
 pub struct DummyNavigationProvider;
 
 impl NavigationProvider for DummyNavigationProvider {
-    fn navigate_new_page(&self, _url: String) {
+    fn navigate_new_page(&self, _navigation: NavigationOptions) {
         // Default impl: do nothing
+    }
+}
+
+#[non_exhaustive]
+#[derive(Debug, Clone)]
+pub struct NavigationOptions {
+    /// The URL to navigate to
+    pub url: Url,
+
+    /// Source document for the navigation
+    pub source_document: usize,
+
+    pub referrer_policy: String,
+}
+
+impl Default for NavigationOptions {
+    fn default() -> Self {
+        Self {
+            url: Url::parse("http://localhost").unwrap(),
+            source_document: 0,
+            referrer_policy: String::new(),
+        }
+    }
+}
+
+impl NavigationOptions {
+    pub fn new(url: Url, source_document: usize) -> Self {
+        Self {
+            url,
+            source_document,
+            ..Default::default()
+        }
+    }
+    pub fn with_referrer_policy(mut self, resource: String) -> Self {
+        self.referrer_policy = resource;
+        self
     }
 }
