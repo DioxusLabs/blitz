d7aef5901554d6f6cf8daa94a5fcce98a1b44238
Nico Burns <nico@nicoburns.com>
2025-01-19 18:02:45 +1300

    Bump AccessKit version
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- packages/blitz-dom/src/accessibility.rs
+++ packages/blitz-dom/src/accessibility.rs
@@ -1,10 +1,10 @@
-use crate::{local_name, BaseDocument, Node};
-use accesskit::{NodeBuilder, NodeId, Role, Tree, TreeUpdate};
+use crate::{local_name, BaseDocument, Node as BlitzDomNode};
+use accesskit::{Node as AccessKitNode, NodeId, Role, Tree, TreeUpdate};
 
 impl BaseDocument {
     pub fn build_accessibility_tree(&self) -> TreeUpdate {
         let mut nodes = std::collections::HashMap::new();
-        let mut window = NodeBuilder::new(Role::Window);
+        let mut window = AccessKitNode::new(Role::Window);
 
         self.visit(|node_id, node| {
             let parent = node
@@ -12,34 +12,36 @@ impl BaseDocument {
                 .and_then(|parent_id| nodes.get_mut(&parent_id))
                 .map(|(_, parent)| parent)
                 .unwrap_or(&mut window);
-            let (id, node_builder) = self.build_accessibility_node(node, parent);
+            let (id, builder) = self.build_accessibility_node(node, parent);
 
-            nodes.insert(node_id, (id, node_builder));
+            nodes.insert(node_id, (id, builder));
         });
 
         let mut nodes: Vec<_> = nodes
             .into_iter()
-            .map(|(_, (id, node))| (id, node.build()))
+            .map(|(_, (id, node))| (id, node))
             .collect();
-        nodes.push((NodeId(0), window.build()));
+        nodes.push((NodeId(u64::MAX), window));
 
-        let tree = Tree::new(NodeId(0));
+        let tree = Tree::new(NodeId(u64::MAX));
         TreeUpdate {
             nodes,
             tree: Some(tree),
-            focus: NodeId(0),
+            focus: NodeId(self.focus_node_id.map(|id| id as u64).unwrap_or(u64::MAX)),
         }
     }
 
     fn build_accessibility_node(
         &self,
-        node: &Node,
-        parent: &mut NodeBuilder,
-    ) -> (NodeId, NodeBuilder) {
+        node: &BlitzDomNode,
+        parent: &mut AccessKitNode,
+    ) -> (NodeId, AccessKitNode) {
         let id = NodeId(node.id as u64);
 
-        let mut node_builder = NodeBuilder::default();
-        if let Some(element_data) = node.element_data() {
+        let mut builder = AccessKitNode::default();
+        if node.id == 0 {
+            builder.set_role(Role::Window)
+        } else if let Some(element_data) = node.element_data() {
             let name = element_data.name.local.to_string();
 
             // TODO match more roles
@@ -61,16 +63,16 @@ impl BaseDocument {
                 _ => Role::Unknown,
             };
 
-            node_builder.set_role(role);
-            node_builder.set_html_tag(name);
+            builder.set_role(role);
+            builder.set_html_tag(name);
         } else if node.is_text_node() {
-            node_builder.set_role(Role::StaticText);
-            node_builder.set_name(node.text_content());
+            builder.set_role(Role::TextRun);
+            builder.set_value(node.text_content());
             parent.push_labelled_by(id)
         }
 
         parent.push_child(id);
 
-        (id, node_builder)
+        (id, builder)
     }
 }
