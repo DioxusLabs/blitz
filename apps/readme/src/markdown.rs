c0c59a3f6bad64e204264d62d35a8d79b61d9f6b
Nico Burns <nico@nicoburns.com>
2025-01-25 09:37:04 +1300

    Bump comrak to v0.35 (support info/warning/etc blocks)
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- apps/readme/src/markdown.rs
+++ apps/readme/src/markdown.rs
@@ -25,6 +25,7 @@ pub(crate) fn markdown_to_html(contents: String) -> String {
                 description_lists: false,
                 front_matter_delimiter: None,
                 multiline_block_quotes: false,
+                alerts: true,
                 ..ExtensionOptions::default()
             },
             render: RenderOptions {
