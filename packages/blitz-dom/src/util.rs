7ca20c995d66db5322cbebfd2fa4b6d30af65893
koko <kokos.kekse@gmail.com>
2025-03-04 00:58:38 +0100

    Use custom iterator instead of node_chain
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- packages/blitz-dom/src/util.rs
+++ packages/blitz-dom/src/util.rs
@@ -123,15 +123,20 @@ impl ToColorColor for AbsoluteColor {
     }
 }
 
+#[derive(Clone)]
+/// An pre-order tree traverser for a [BaseDocument](crate::document::BaseDocument).
 pub struct TreeTraverser<'a> {
     doc: &'a BaseDocument,
     stack: Vec<usize>,
 }
 
 impl<'a> TreeTraverser<'a> {
+    /// Creates a new tree traverser for the given document which starts at the root node.
     pub fn new(doc: &'a BaseDocument) -> Self {
         Self::new_with_root(doc, 0)
     }
+
+    /// Creates a new tree traverser for the given document which starts at the specified node.
     pub fn new_with_root(doc: &'a BaseDocument, root: usize) -> Self {
         TreeTraverser {
             doc,
@@ -147,5 +152,30 @@ impl Iterator for TreeTraverser<'_> {
         let node = self.doc.get_node(id)?;
         self.stack.extend(node.children.iter().rev());
         Some(id)
+    }
+}
+
+#[derive(Clone)]
+/// An ancestor traverser for a [BaseDocument](crate::document::BaseDocument).
+pub struct AncestorTraverser<'a> {
+    doc: &'a BaseDocument,
+    current: usize,
+}
+impl<'a> AncestorTraverser<'a> {
+    /// Creates a new ancestor traverser for the given document and node ID.
+    pub fn new(doc: &'a BaseDocument, node_id: usize) -> Self {
+        AncestorTraverser {
+            doc,
+            current: node_id,
+        }
+    }
+}
+impl Iterator for AncestorTraverser<'_> {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let current_node = self.doc.get_node(self.current)?;
+        self.current = current_node.parent?;
+        Some(self.current)
     }
 }
