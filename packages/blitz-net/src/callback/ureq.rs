d11fe9e90600a4abc42013e2b9f1d4d4b0928106
SpikeHD <25207995+SpikeHD@users.noreply.github.com>
2025-03-05 07:52:03 -0800

    feat: clippy, mpsc channel for ureq

--- packages/blitz-net/src/callback/ureq.rs
+++ packages/blitz-net/src/callback/ureq.rs
@@ -1,1 +1,16 @@
+use blitz_traits::net::NetCallback;
+use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
 
+pub struct MpscCallback<T>(SyncSender<(usize, T)>);
+impl<T> MpscCallback<T> {
+    pub fn new() -> (Receiver<(usize, T)>, Self) {
+        let (send, recv) = sync_channel(0);
+        (recv, Self(send))
+    }
+}
+impl<T: Send + Sync + 'static> NetCallback for MpscCallback<T> {
+    type Data = T;
+    fn call(&self, doc_id: usize, data: Self::Data) {
+        let _ = self.0.send((doc_id, data));
+    }
+}
