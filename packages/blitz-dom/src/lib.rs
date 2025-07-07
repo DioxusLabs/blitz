2eec835234eec863a1b611be083fb82d30ed0635
Nico Burns <nico@nicoburns.com>
2025-07-07 15:13:14 +0100

    Improve `blitz-dom` lib.rs documentation

--- packages/blitz-dom/src/lib.rs
+++ packages/blitz-dom/src/lib.rs
@@ -1,27 +1,30 @@
 //! The core DOM abstraction in Blitz
 //!
-//! This crate implements a headless DOM designed to emebedded in and "driven" by external code.
+//! This crate implements a flexible headless DOM ([`BaseDocument`]), which is designed to emebedded in and "driven" by external code. Most users will want
+//! to use a wrapper:
 //!
-//! It includes:
-//!  - A DOM tree respresentation
-//!  - CSS parsing and resolution
-//!  - Layout
-//!  - Event handling
+//!  - [`HtmlDocument`](https://docs.rs/blitz-html/latest/blitz_html/struct.HtmlDocument.html) from the [blitz-html](https://docs.rs/blitz-html) crate.
+//!    Allows you to parse HTML (or XHTML) into a Blitz [`BaseDocument`], and can be combined with a markdown-to-html converter like [comrak](https://docs.rs/comrak)
+//!    or [pulldown-cmark](https://docs.rs/pulldown-cmark) to render/process markdown.
+//!  - [`DioxusDocument`](https://docs.rs/dioxus-native/latest/dioxus_native/struct.DioxusDocument.html) from the [dioxus-native](https://docs.rs/dioxus-native) crate.
+//!    Combines a [`BaseDocument`] with a Dioxus `VirtualDom` to enable dynamic rendering and event handling.
 //!
-//! The following functionality is not included within blitz-dom. However there are extension points that can be used to implement this
-//! functionality using either another `blitz-*` crate or a custom implementation:
-//!  - Networking (see blitz_net)
-//!  - Windowing or an event loop (see blitz_shell)
-//!  - Rendering (see `blitz_paint`)
+//! It includes: A DOM tree respresentation, CSS parsing and resolution, layout and event handling. Additional functionality is available in 
+//! separate crates, including html parsing ([blitz-html](https://docs.rs/blitz-html)), networking ([blitz-net](https://docs.rs/blitz-html)),
+//! rendering ([blitz-paint](https://docs.rs/blitz-paint)) and windowing ([blitz-shell](https://docs.rs/blitz-shell)).
+//!
+//! Most of the functionality in this crates is provided through the  struct.
 //!
 //! `blitz-dom` has a native Rust API that is designed for higher-level abstractions to be built on top (although it can also be used directly).
 //!
 //! The goal behind this crate is that any implementor can interact with the DOM and render it out using any renderer
 //! they want.
 //!
-//! ## Feature flags
-//!  - `default`: Enables the features listed below.
-//!  - `tracing`: Enables tracing support.
+
+// TODO: Document features
+// ## Feature flags
+//  - `default`: Enables the features listed below.
+//  - `tracing`: Enables tracing support.
 
 pub const DEFAULT_CSS: &str = include_str!("../assets/default.css");
 pub(crate) const BULLET_FONT: &[u8] = include_bytes!("../assets/moz-bullet-font.otf");
