e18a8dc92fae6343f756a5050d8bd20e2cec99c8
Gaurav Gautam <gautam1168@gmail.com>
2025-08-30 15:53:23 +0530

    Add more examples

--- examples/outline.rs
+++ examples/outline.rs
@@ -8,34 +8,59 @@ fn main() {
 }
 
 fn app() -> Element {
+    let clip_styles = [
+      // "clip-path: inset(100px 50px);",
+      "clip-path: circle(40%);",
+      "clip-path: ellipse(430px 440px at 40% 10%);",
+      "clip-path: polygon(50% 0, 100% 50%, 50% 100%, 0 50%);",
+      // "clip-path: path(\"M0.5,1 C0.5,1,0,0.7,0,0.3 A0.25,0.25,1,1,1,0.5,0.3 A0.25,0.25,1,1,1,1,0.3 C1,0.7,0.5,1,0.5,1 Z\");",
+      "clip-path: rect(5px 5px 160px 145px round 20%);",
+      // "clip-path: shape(from 0% 0%, line to 100% 0%, line to 50% 100%, close);",
+    ];
+
     rsx! {
         style { {CSS} }
-        img { 
-            class: "image",
-            src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" 
+        div {
+          class: "example",
+          div {
+            class: "imagecontainer",
+            "no clip path"
+            img {
+              class: "image",
+              src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg"
+            }
+          }
+
+          for style in clip_styles {
+            div  {
+              class: "imagecontainer",
+              {style}
+              img {
+                class: "image",
+                style: "{style}",
+                src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg"
+              }
+            }
+          }
         }
     }
 }
 
-// const CSS: &str = r#"
-// .image {
-//     clip-path: circle(40%);
-// }
-// "#;
-
-// const CSS: &str = r#"
-// .image {
-//     clip-path: ellipse(430px 440px at 40% 10%);
-// }
-// "#;
-
-// const CSS: &str = r#"
-// .image {
-//     clip-path: polygon(50% 0, 100% 50%, 50% 100%, 0 50%);
-// }
-
 const CSS: &str = r#"
+.example {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
 .image {
-    clip-path: rect(5px 145px 160px 5px round 20%);
+  border: solid 1px green;
+  width: 300px;
+  height: 300px;
+}
+.imagecontainer {
+  border: solid 1px red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 "#;
