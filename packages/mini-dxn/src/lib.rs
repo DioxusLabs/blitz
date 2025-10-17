52644077abfccbd05612b2ad52ecbbd1c6a762f1
Nico Burns <nico@nicoburns.com>
2025-10-17 10:57:54 +0100

    Enable vello_hybrid for mini-dxn and todomvc
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- packages/mini-dxn/src/lib.rs
+++ packages/mini-dxn/src/lib.rs
@@ -100,7 +100,7 @@ pub fn launch_cfg(
     // Create the renderer
     #[cfg(feature = "gpu")]
     let renderer = DxnWindowRenderer::with_features_and_limits(features, limits);
-    #[cfg(feature = "cpu-base")]
+    #[cfg(any(feature = "cpu-base", feature = "hybrid"))]
     let renderer = DxnWindowRenderer::new();
 
     // Spin up the virtualdom
