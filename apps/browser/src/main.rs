7335999ef42d2f7cefe70ccebe9b0ab8d9725b05
Nico Burns <nico@nicoburns.com>
2025-11-22 17:49:44 +0000

    Use a Store to manage URL and History state

--- apps/browser/src/main.rs
+++ apps/browser/src/main.rs
@@ -26,48 +26,42 @@ fn main() {
     dioxus_native::launch(app)
 }
 
+type SyncStore<T> = Store<T, CopyValue<T, SyncStorage>>;
+
+fn use_sync_store<T: Send + Sync + 'static>(value: impl FnOnce() -> T) -> SyncStore<T> {
+    use_hook(|| Store::new_maybe_sync(value()))
+}
+
 fn app() -> Element {
     let home_url = use_hook(|| String::from("https://wikipedia.org"));
 
     let mut url_input_value = use_signal(|| home_url.clone());
-    let history: SyncSignal<Vec<String>> = use_signal_sync(|| Vec::new());
-    let mut url: SyncSignal<Option<String>> = use_signal_sync(|| Some(url_input_value()));
+    let mut history: SyncStore<History> = use_sync_store(|| History::new(home_url.clone()));
 
     let net_provider = use_context::<Arc<StdNetProvider>>();
-    let loader = use_hook(|| DocumentLoader::new(net_provider, url.clone(), history.clone()));
+    let loader = use_hook(|| DocumentLoader::new(net_provider, history.clone()));
     let content_doc = loader.doc.clone();
 
-    let refresh = use_callback(move |_| {
-        if let Some(url_s) = url() {
-            println!("Loading {}...", url_s);
-            if let Ok(url) = Url::parse(&url_s) {
-                *url_input_value.write_unchecked() = url_s;
-                loader.load_document(url);
-            } else if let Ok(url) = Url::parse(&format!("https://{url_s}")) {
-                *url_input_value.write_unchecked() = format!("https://{url_s}");
-                loader.load_document(url);
-            } else {
-                println!("Error parsing URL {}", url_s);
-            }
+    let load_current_url = use_callback(move |_| {
+        let url_s = history.current_url().read().clone();
+        println!("Loading {}...", &url_s);
+        if let Ok(url) = Url::parse(&url_s) {
+            *url_input_value.write_unchecked() = url_s.clone();
+            loader.load_document(url);
+        } else if let Ok(url) = Url::parse(&format!("https://{}", &url_s)) {
+            *url_input_value.write_unchecked() = format!("https://{}", &url_s);
+            loader.load_document(url);
+        } else {
+            println!("Error parsing URL {}", &url_s);
         }
     });
 
-    use_effect(move || refresh(()));
+    use_effect(move || load_current_url(()));
 
-    let back_action = use_callback(move |_| {
-        if let Some(prev) = history.write_unchecked().pop() {
-            *url.write_unchecked() = Some(prev);
-        }
-    });
-
-    let refresh_action = refresh;
-
-    let home_action = use_callback(move |_| {
-        if let Some(prev) = url.read().as_ref() {
-            history.write_unchecked().push(prev.clone());
-        }
-        *url.write_unchecked() = Some(home_url.clone())
-    });
+    let back_action = use_callback(move |_| history.go_back());
+    let forward_action = use_callback(move |_| history.go_forward());
+    let home_action = use_callback(move |_| history.navigate_to(home_url.clone()));
+    let refresh_action = load_current_url;
 
     rsx!(
         div { id: "frame",
@@ -77,7 +71,7 @@ fn app() -> Element {
             // Toolbar
             div { class: "urlbar",
                 IconButton { icon: icons::BACK_ICON, action: back_action }
-                IconButton { icon: icons::FORWARDS_ICON }
+                IconButton { icon: icons::FORWARDS_ICON, action: forward_action }
                 IconButton { icon: icons::REFRESH_ICON, action: refresh_action }
                 IconButton { icon: icons::HOME_ICON, action: home_action }
                 input {
@@ -88,7 +82,7 @@ fn app() -> Element {
                     onkeydown: move |evt| {
                         if evt.key() == Key::Enter {
                             evt.prevent_default();
-                            *url.write() = Some(url_input_value());
+                            history.navigate_to(url_input_value());
                         }
                     },
                     oninput: move |evt| { *url_input_value.write() = evt.value() },
@@ -102,19 +96,76 @@ fn app() -> Element {
     )
 }
 
+#[derive(Store)]
+struct History {
+    urls: Vec<String>,
+    current: usize,
+}
+
+impl History {
+    fn new(initial_url: String) -> Self {
+        Self {
+            urls: vec![initial_url],
+            current: 0,
+        }
+    }
+}
+
+#[store]
+impl<Lens> Store<History, Lens> {
+    fn current_idx(&self) -> usize {
+        *self.current().read()
+    }
+
+    fn current_url(&self) -> impl Readable<Target = String> {
+        self.urls().get(self.current_idx()).unwrap()
+    }
+
+    fn has_back(&self) -> bool {
+        self.current_idx() > 0
+    }
+
+    fn has_forward(&self) -> bool {
+        self.current_idx() < self.urls().len() - 1
+    }
+
+    fn go_back(&mut self) {
+        if self.has_back() {
+            *self.current().write() -= 1;
+        }
+    }
+
+    fn go_forward(&mut self) {
+        if self.has_forward() {
+            *self.current().write() += 1;
+        }
+    }
+
+    fn navigate_to(&self, new_url: String)
+    where
+        Lens: Writable,
+    {
+        let idx = self.current_idx();
+        self.urls().write().truncate(idx + 1);
+        self.urls().push(new_url);
+        *self.current().write() += 1;
+    }
+
+    fn refresh(&mut self) {
+        // Trigger change detection without actually changing the URL
+        let _ = self.current().write();
+    }
+}
+
 struct BrowserNavProvider {
-    url_signal: SyncSignal<Option<String>>,
-    history: SyncSignal<Vec<String>>,
+    history: SyncStore<History>,
 }
 
 impl NavigationProvider for BrowserNavProvider {
     fn navigate_to(&self, options: NavigationOptions) {
         if options.method == Method::GET {
-            if let Some(prev) = self.url_signal.read().as_ref() {
-                self.history.write_unchecked().push(prev.clone());
-            }
             let url = options.url.to_string();
-            *self.url_signal.write_unchecked() = Some(url.clone());
+            self.history.navigate_to(url);
         }
     }
 }
@@ -129,8 +180,7 @@ struct DocumentLoader {
     status: Signal<DocumentLoaderStatus>,
     request_id_counter: AtomicUsize,
     doc: Signal<Option<SubDocumentAttr>>,
-    url_signal: SyncSignal<Option<String>>,
-    history: SyncSignal<Vec<String>>,
+    history: SyncStore<History>,
 }
 
 impl Clone for DocumentLoader {
@@ -140,24 +190,18 @@ impl Clone for DocumentLoader {
             status: self.status.clone(),
             request_id_counter: AtomicUsize::new(self.request_id_counter.load(Ao::SeqCst)),
             doc: self.doc.clone(),
-            url_signal: self.url_signal.clone(),
             history: self.history.clone(),
         }
     }
 }
 
 impl DocumentLoader {
-    fn new(
-        net_provider: Arc<StdNetProvider>,
-        url_signal: SyncSignal<Option<String>>,
-        history: SyncSignal<Vec<String>>,
-    ) -> Self {
+    fn new(net_provider: Arc<StdNetProvider>, history: SyncStore<History>) -> Self {
         Self {
             net_provider,
             status: Signal::new(DocumentLoaderStatus::Idle),
             request_id_counter: AtomicUsize::new(0),
             doc: Signal::new(None),
-            url_signal,
             history,
         }
     }
@@ -166,7 +210,6 @@ impl DocumentLoader {
         let request_id = self.request_id_counter.fetch_add(1, Ao::Relaxed);
         let net_provider = Arc::clone(&self.net_provider);
         let doc_signal = self.doc.clone();
-        let url_signal = self.url_signal.clone();
         let history = self.history.clone();
         let task = spawn(async move {
             let request = net_provider.fetch_async(Request::get(url));
@@ -178,10 +221,7 @@ impl DocumentLoader {
                         base_url: Some(resolved_url),
                         ua_stylesheets: None,
                         net_provider: Some(net_provider as _), // FIXME
-                        navigation_provider: Some(Arc::new(BrowserNavProvider {
-                            url_signal,
-                            history,
-                        })),
+                        navigation_provider: Some(Arc::new(BrowserNavProvider { history })),
                         shell_provider: Some(consume_context::<Arc<dyn ShellProvider>>()),
                         html_parser_provider: Some(Arc::new(HtmlProvider)),
                         font_ctx: None,
