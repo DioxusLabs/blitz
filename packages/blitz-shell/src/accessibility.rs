43b6c5dac087caf532cf749f791c2e851944cf88
Nico Burns <nico@nicoburns.com>
2025-12-08 16:08:53 +0000

    Fix warnings
    Signed-off-by: Nico Burns <nico@nicoburns.com>

--- packages/blitz-shell/src/accessibility.rs
+++ packages/blitz-shell/src/accessibility.rs
@@ -1,7 +1,7 @@
-use crate::event::{BlitzShellEvent, BlitzShellProxy};
+use crate::event::BlitzShellProxy;
 // use accesskit_winit::Adapter;
 use blitz_dom::BaseDocument;
-use winit::{event_loop::EventLoopProxy, window::Window};
+use winit::window::Window;
 
 /// State of the accessibility node tree and platform adapter.
 pub struct AccessibilityState {
@@ -11,11 +11,14 @@ pub struct AccessibilityState {
 
 impl AccessibilityState {
     pub fn new(window: &dyn Window, proxy: BlitzShellProxy) -> Self {
+        let _ = window;
+        let _ = proxy;
         Self {
             // adapter: Adapter::with_event_loop_proxy(window, proxy.clone()),
         }
     }
     pub fn update_tree(&mut self, doc: &BaseDocument) {
+        let _ = doc;
         // self.adapter
         // .update_if_active(|| doc.build_accessibility_tree());
     }
