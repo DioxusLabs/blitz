5456f427a6e7b49ad79f867e9b0c0438626617f9
Austin M. Reppert <austinmreppert@protonmail.com>
2026-03-06 00:08:01 -0500

    Add word-spacing.

--- packages/blitz-dom/src/stylo_to_parley.rs
+++ packages/blitz-dom/src/stylo_to_parley.rs
@@ -125,6 +125,11 @@ pub(crate) fn style(
         .resolve(Length::new(font_size))
         .px();
 
+    let word_spacing = itext_styles
+        .word_spacing
+        .resolve(Length::new(font_size))
+        .px();
+
     // Convert Bold/Italic
     let font_weight = self::font_weight(font_styles.font_weight);
     let font_style = self::font_style(font_styles.font_style);
@@ -192,7 +197,7 @@ pub(crate) fn style(
         font_features: parley::FontFeatures::List(Cow::Borrowed(&[])),
         locale: Default::default(),
         line_height,
-        word_spacing: Default::default(),
+        word_spacing,
         letter_spacing,
         text_wrap_mode,
         overflow_wrap,
