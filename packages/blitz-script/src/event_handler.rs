f3c584de8fb9db4f6e2c1249b33e441104d45b67
Nico Burns <nico@nicoburns.com>
2026-07-05 14:33:34 +0100

    Initial JS engine implementation

---
+++ packages/blitz-script/src/event_handler.rs
@@ -0,0 +1,24 @@
+//! Integration with blitz-dom's event driver
+
+use blitz_dom::{Document, EventHandler, NodeId};
+use blitz_traits::events::{DomEvent, EventState};
+
+use crate::runtime::ScriptRuntime;
+
+/// An [`EventHandler`] which dispatches DOM events to JavaScript event listeners
+/// before Blitz's default actions run.
+pub(crate) struct ScriptEventHandler<'rt> {
+    pub runtime: &'rt mut ScriptRuntime,
+}
+
+impl EventHandler for ScriptEventHandler<'_> {
+    fn handle_event(
+        &mut self,
+        chain: &[NodeId],
+        event: &mut DomEvent,
+        _doc: &mut dyn Document,
+        event_state: &mut EventState,
+    ) {
+        self.runtime.dispatch_dom_event(chain, event, event_state);
+    }
+}
