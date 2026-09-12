6eedc389c99b6fc2a6f7820caa5c242ecb812c3a
Nico Burns <nico@nicoburns.com>
2026-09-12 22:49:17 +0100

    script: make element.style assignable (PutForwards=cssText); enable sibling-index()/sibling-count() (#865)

--- packages/blitz-dom/src/document.rs
+++ packages/blitz-dom/src/document.rs
@@ -399,6 +399,7 @@ impl BaseDocument {
         style_config::set_pref!("layout.columns.enabled", true);
         style_config::set_pref!("layout.css.basic-shape-shape.enabled", true);
         style_config::set_pref!("layout.css.attr.enabled", true);
+        style_config::set_pref!("layout.css.tree-counting-functions.enabled", true);
         style_config::set_pref!("layout.threads", -1);
 
         let viewport = config.viewport.unwrap_or_default();
