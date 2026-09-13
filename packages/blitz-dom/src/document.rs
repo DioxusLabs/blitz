0b05eca96199bdb34512c1807af48cb973225c49
Nico Burns <nico@nicoburns.com>
2026-09-13 01:18:17 +0100

    Enable stylo's layout.variable_fonts.enabled pref (font-variation-settings, font-optical-sizing) (#876)

--- packages/blitz-dom/src/document.rs
+++ packages/blitz-dom/src/document.rs
@@ -401,6 +401,7 @@ impl BaseDocument {
         style_config::set_pref!("layout.css.attr.enabled", true);
         style_config::set_pref!("layout.css.tree-counting-functions.enabled", true);
         style_config::set_pref!("layout.css.progress-function.enabled", true);
+        style_config::set_pref!("layout.variable_fonts.enabled", true);
         style_config::set_pref!("layout.threads", -1);
 
         let viewport = config.viewport.unwrap_or_default();
